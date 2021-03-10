1084ddeb9baace1fe512c3c4fd398188c9b46b6d
Diederick Verweij <diederick.verweij@gmail.com>
2021-03-10 09:21:27 +0100

    chore: simplify app: remove fragments

--- app/src/main/java/com/assignment/jakewharton/EventsActivity.kt
+++ app/src/main/java/com/assignment/jakewharton/EventsActivity.kt
@@ -1,32 +1,97 @@
 package com.assignment.jakewharton
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import com.assignment.jakewharton.config.REPO_KEY
+import com.assignment.jakewharton.databinding.EventsActivityBinding
 import com.assignment.jakewharton.model.Repo
-import com.assignment.jakewharton.ui.main.EventsFragment
+import com.assignment.jakewharton.ui.main.EventResponse
+import com.assignment.jakewharton.ui.main.TributeEventsInfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.net.URL
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class EventsActivity : AppCompatActivity() {
 
+    private lateinit var binding : EventsActivityBinding
+
+    private val viewModel: TributeEventsInfoViewModel by viewModels()
+
+    private lateinit var repo: Repo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        repo = intent.getParcelableExtra<Repo>(REPO_KEY)!!
         setTitle(R.string.events_title)
-        setContentView(R.layout.events_activity)
 
-        intent?.also { intent ->
-            val repo = intent.getParcelableExtra<Repo>(REPO_KEY)
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, EventsFragment.newInstance(repo!!))
-                .commitNow()
+        binding = EventsActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        observeViewModel()
+        viewModel.fetchEvents(repo)
+    }
+
+    private fun observeViewModel() {
+        viewModel.eventLiveData.observe(this) { response ->
+            when (response) {
+                is EventResponse.Loading -> {
+                    binding.progress.visibility = View.VISIBLE
+                    binding.error.visibility = View.GONE
+                }
+                is EventResponse.Success -> {
+                    binding.apply {
+                        progress.visibility = View.GONE
+                        error.visibility = View.GONE
+                        ownerValue.text = repo.owner.login
+                        ownerUrlValue.text = crop(repo.owner.url)
+                        ownerUrlValue.setOnClickListener {
+                            openURL(repo.owner.url)
+                        }
+                        repoValue.text = repo.name
+                        repoUrlValue.text = crop(repo.htmlUrl)
+                        repoUrlValue.setOnClickListener {
+                            openURL(repo.htmlUrl)
+                        }
+
+                        if (response.result.isEmpty().not()) {
+                            response.result.sortedBy { event ->
+                                event.created.time
+                            }.last().apply {
+                                eventTypeValue.text = type
+                                eventActorValue.text = actor.displayLogin
+                                eventActorUrlValue.text = crop(actor.url)
+                                eventActorUrlValue.setOnClickListener {
+                                    openURL(actor.url)
+                                }
+                            }
+                        }
+                    }
+                }
+                is EventResponse.Error -> {
+                    binding.error.text = response.message
+                    binding.error.visibility = View.VISIBLE
+                    binding.progress.visibility = View.GONE
+                }
+            }
         }
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
+    }
+
+    private fun crop(urlString: String): String {
+        return URL(urlString).path
+    }
+
+    private fun openURL(url: String) {
+        val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(myIntent)
     }
 }
--- app/src/main/java/com/assignment/jakewharton/RepoActivity.kt
+++ app/src/main/java/com/assignment/jakewharton/RepoActivity.kt
@@ -1,9 +1,17 @@
 package com.assignment.jakewharton
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.assignment.jakewharton.adapter.RepoListAdapter
 import com.assignment.jakewharton.config.GITHUB_OWNERS
-import com.assignment.jakewharton.ui.main.RepoFragment
+import com.assignment.jakewharton.config.REPO_KEY
+import com.assignment.jakewharton.databinding.RepoActivityBinding
+import com.assignment.jakewharton.ui.main.RepoResponse
+import com.assignment.jakewharton.ui.main.TributeInfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -11,14 +19,49 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class RepoActivity : AppCompatActivity() {
 
+    private lateinit var binding : RepoActivityBinding
+
+    private val viewModel: TributeInfoViewModel by viewModels()
+
+    val repoListAdapter = RepoListAdapter() { repo ->
+        val intent = Intent(this, EventsActivity::class.java).apply {
+            putExtra(REPO_KEY, repo)
+        }
+        startActivity(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTitle(GITHUB_OWNERS.map { s -> s.capitalize() }.reduceRight { a, b -> "Repos of $a & $b"})
-        setContentView(R.layout.repo_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, RepoFragment.newInstance())
-                .commitNow()
+
+        binding = RepoActivityBinding.inflate(layoutInflater).apply {
+            recyclerView.adapter = repoListAdapter
+            recyclerView.layoutManager = LinearLayoutManager(this@RepoActivity)
+        }
+        setContentView(binding.root)
+
+        observeViewModel()
+        viewModel.fetchRepos()
+    }
+
+    private fun observeViewModel() {
+        viewModel.reposLiveData.observe(this) { response ->
+            when (response) {
+                is RepoResponse.Loading -> {
+                    binding.progress.visibility = View.VISIBLE
+                    binding.error.visibility = View.GONE
+                }
+                is RepoResponse.Success -> {
+                    binding.progress.visibility = View.GONE
+                    binding.error.visibility = View.GONE
+                    repoListAdapter.setItems(response.result)
+                }
+                is RepoResponse.Error -> {
+                    binding.error.text = response.message
+                    binding.error.visibility = View.VISIBLE
+                    binding.progress.visibility = View.GONE
+                }
+            }
         }
     }
 }
